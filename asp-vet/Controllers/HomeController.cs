ab81a55968da33193174c62a2a807a25f937e4e0
valen <valenpschool@gmail.com>
2022-05-12 21:53:43 -0300

    Adicionar HomeController

--- asp-vet/Controllers/HomeController.cs
+++ asp-vet/Controllers/HomeController.cs
@@ -1,30 +1,187 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using asp_vet.Dados;
+using asp_vet.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace asp_vet.Controllers
 {
     public class HomeController : Controller
     {
+        AcoesLogin acL = new AcoesLogin();
+        AcoesUser acTipo = new AcoesUser();
+
+        public void carregaTipoUsu()
+        {
+            List<SelectListItem> tipoUsu = new List<SelectListItem>();
+
+            using (MySqlConnection con = new MySqlConnection("Server=localhost;DataBase=bdVeterinaria;User=root;pwd=12345678"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from tbTipoUsuario", con);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    tipoUsu.Add(new SelectListItem
+                    {
+                        Text = rdr[1].ToString(),
+                        Value = rdr[0].ToString()
+                    });
+                }
+                con.Close();
+
+            }
+            ViewBag.tipoUsu = new SelectList(tipoUsu, "Value", "Text");
+        }
+
         public ActionResult Index()
         {
+            Session["tipo"] = "";
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(ModelLogin cm)
         {
-            ViewBag.Message = "Your application description page.";
+            acL.TestarUsuario(cm);
 
+            if (cm.usuario != null)
+            {
+                FormsAuthentication.SetAuthCookie(cm.usuario, false);
+                Session["usu"] = cm.usuario;
+                Session["tipo"] = cm.codTipoUsuario;
+
+                return RedirectToAction("MenuAdm", "Home");
+            }
+       
+            else
+            {
+                ViewBag.msgLogar = "Usuário e/ou senha incorreto(s)";
+                return View();
+            }
+        }
+
+        public ActionResult SemAcesso()
+        {
+            Response.Write("<script>alert('Sem Acesso - Faça o login no sistema')</script>");
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Logout()
         {
-            ViewBag.Message = "Your contact page.";
+            Session["usu"] = null;
+            Session["tipo"] = null;
+            return RedirectToAction("Index", "Home");
+        }
 
+        public ActionResult CadTipo()
+        {
+            if (Session["usu"] != null && Session["tipo"].ToString() == "1")
+            {
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("SemAcesso", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CadTipo(ModelUser cmTipo)
+        {
+            acTipo.inserirTipo(cmTipo);
+            ViewBag.msgCad = "Cadastro Efetuado";
             return View();
+        }
+
+        public ActionResult ConsCadTipo()
+        {
+            if (Session["usu"] != null && Session["tipo"].ToString() == "1")
+            {
+                GridView dgv = new GridView();
+                dgv.DataSource = acTipo.consultaTipo();
+                dgv.DataBind();
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new HtmlTextWriter(sw);
+                dgv.RenderControl(htw);
+                ViewBag.GridViewString = sw.ToString();
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("SemAcesso", "Home");
+            }
+        }
+
+        public ActionResult CadLogin()
+        {
+            if (Session["usu"] != null && Session["tipo"].ToString() == "1")
+            {
+                carregaTipoUsu();
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("SemAcesso", "Home");
+            }
+        }
+        [HttpPost]
+        public ActionResult CadLogin(ModelLogin cmLogin)
+        {
+            carregaTipoUsu();
+            if (cmLogin.senha == cmLogin.confirmaSenha)
+            {
+
+                cmLogin.codTipoUsuario = Request["tipoUsu"];
+                acL.inserirLogin(cmLogin);
+                Response.Write("<script>alert('Cadastro efetuado com sucesso!')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('As senhas não estão iguais.')</script>");
+            }
+            return View();
+        }
+
+        public ActionResult MenuAdm()
+        {
+            if (Session["usu"] != null && Session["tipo"].ToString() == "1")
+            {
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("SemAcesso", "Home");
+            }
+            
+
+        } 
+        public ActionResult MenuUser()
+        {
+            if (Session["usu"] != null && Session["tipo"].ToString() == "2")
+            {
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("SemAcesso", "Home");
+            }
+            
+
         }
     }
+
+    
 }
