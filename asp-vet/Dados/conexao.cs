551aa707f95023bbea78023061f21926d692b950
valen <valenpschool@gmail.com>
2022-05-12 21:11:06 -0300

    Classe de conexão

---
+++ asp-vet/Dados/conexao.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp_vet.Dados
+{
+    public class conexao
+    {
+        MySqlConnection cn = new MySqlConnection("Server=localhost; DataBase=bdVeterinaria; User=root;pwd=12345678");
+        public static string msg;
+
+        public MySqlConnection MyConectarBD()
+        {
+
+            try
+            {
+                cn.Open();
+            }
+
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return cn;
+        }
+
+        public MySqlConnection MyDesconectarBD()
+        {
+
+            try
+            {
+                cn.Close();
+            }
+
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return cn;
+        }
+    }
+}
